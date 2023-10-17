aab06131f432227e26835e95db51bc85ece63d9c
Trhe_piernas <118993644+trhepiernas-hub@users.noreply.github.com>
2023-10-17 13:49:12 +0200

    Add files via upload

---
+++ src/test/java/com/is/scraping/scraping/ScrapingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.is.scraping.scraping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
